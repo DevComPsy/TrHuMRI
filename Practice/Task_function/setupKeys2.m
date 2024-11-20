8e9b8209291b20534372aa9e98d1bac6611517f4
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2024-11-20 15:29:22 +0100

    Add files via upload

---
+++ Practice/Task_function/setupKeys2.m
@@ -0,0 +1,26 @@
+function [escapeKey, leftKey, rightKey, triggerscan,confirm] = setupKeys2()
+% Set up the keys
+% Input: 
+% None
+%
+% Output: 
+% escapeKey,leftKey,rightKey: definitions of keyboard keys
+% triggerscan:                trigger key from scanner
+
+% update 30.04.2025 added KbName('UnifyKeyNames'); 
+%----------------------------------------------------------------------
+%                       Keyboard information
+%----------------------------------------------------------------------
+
+% Define the keyboard keys that are listened for. We will be using the left
+% and right arrow keys as response keys for the task and the escape key as
+% a exit/reset key
+KbName('UnifyKeyNames'); 
+escapeKey = KbName('ESCAPE');
+leftKey = KbName('LeftArrow'); %yellow
+rightKey = KbName('RightArrow'); %green
+triggerscan = KbName('t');
+confirm = KbName('space');
+
+
+end
